76db94442eb7c96f78020ca34a6a257fe2207242
Samuel Dudík <24730635+woef@users.noreply.github.com>
2017-12-26 00:02:58 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,41 +1,77 @@
 var posts = [];
+var after = '';
+var loading;
 
 const app = new Vue({
   el: "#app",
   data: {
-    posts: []
+    posts: [],
+    active: -1
+  },
+  watch: {
+    active: function(active) {
+      if (active != -1) {
+        document.body.style.overflowY = 'hidden';
+      } else {
+        document.body.style.overflowY = 'auto';
+      }
+    }
   },
   created () {
+    sw = screen.width;
+    sh = screen.height;
     window.addEventListener('resize', () => {
       this.posts = [];
       resized();
     });
-    axios.get('https://www.reddit.com/r/aww+aww_gifs+hardcoreaww/.json?limit=50')
-      .then(res => {
-        res.data.data.children.forEach(child => {
-          if (child.data.domain != 'reddit.com' && child.data.domain != 'v.redd.it') {
-            if (!child.data.preview.images[0].variants.mp4) {
-              data = child.data.preview.images[0].resolutions[2];
-              data.type = 'image';
-            } else {
-              if (child.data.preview.images[0].variants.mp4.resolutions.length > 2) {
-                data = child.data.preview.images[0].variants.mp4.resolutions[2];
-              } else {
-                data = child.data.preview.images[0].variants.mp4.source;
-              }
-              data.type = 'video';
-            }
-            data.url = data.url.replace(/amp;/g, '');
-            posts.push(data);
-          }
-        });
-        resized();
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    window.addEventListener('keypress', (e) => {
+      keyCode = e.keyCode;
+      if (this.active != -1) {
+        if (keyCode == 39 && this.active != this.posts.length - 1) {
+          this.active++;
+        } else if (keyCode == 37) {
+          this.active--;
+        } else if (keyCode == 27) {
+          this.active = -1;
+        }
+      }
+    });
+    window.addEventListener('scroll', () => {
+      if (window.innerHeight + window.scrollY == document.body.offsetHeight && !loading) {
+        load();
+      }
+    });
+    load();
   }
 });
+
+function load() {
+  loading = true;
+  axios.get('https://www.reddit.com/r/aww+aww_gifs+hardcoreaww/.json?limit=50&after=' + after)
+    .then(res => {
+      res.data.data.children.forEach(child => {
+        if (child.data.domain != 'reddit.com' && child.data.domain != 'v.redd.it' && child.data.preview) {
+          if (!child.data.preview.images[0].variants.mp4) {
+            base = child.data.preview.images[0].resolutions;
+            type = 'image';
+          } else {
+            base = child.data.preview.images[0].variants.mp4.resolutions;
+            type = 'video';
+          }
+          data = base[2] || base[1] || base[0];
+          data.type = type;
+          data.source = base[base.length - 1].url.replace(/amp;/g, '');
+          data.url = data.url.replace(/amp;/g, '');
+          posts.push(data);
+        }
+      });
+      after = res.data.data.after;
+      resized();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
 
 function resized() {
   cw = document.getElementById('app').offsetWidth - 6.1;
@@ -52,7 +88,9 @@ function resized() {
       });
       app.posts = app.posts.concat(temp);
       temp = [];
+      posts = [];
       aspRatAll = 0;
+      loading = false;
     }
   });
 }
